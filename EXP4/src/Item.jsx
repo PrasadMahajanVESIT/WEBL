a1b210849781861d68ab79df2444b07e0a93b03b
prasad0706 <prasad070606@gmail.com>
2026-03-26 11:16:50 +0530

    Refactor App and Item components for improved readability and consistency

--- EXP4/src/Item.jsx
+++ EXP4/src/Item.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function Item({ data }) {
-
   const [show, setShow] = useState(false);
-
   return (
     <div className="card">
       <h3>{data.title}</h3>
-
       <button onClick={() => setShow(!show)}>
         {show ? "Hide Description" : "Show Description"}
       </button>
-
       {show && <p>{data.body}</p>}
     </div>
   );
 }
 
 export default Item;
+
--- EXP5/src/App.jsx
+++ EXP5/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FormComponent from "./FormComponent";
 
 function App() {
